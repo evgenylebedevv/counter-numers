27d30b94caa049e6b830c6556da840c45a6a518a
Evgeny Lebedev <EvgenyLebedevv@gmail.com>
2023-04-08 18:49:11 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: this.props.counter
+    }
+  }
+
+  incCounter = () => {
+    if (this.state.counter < 50) {
+      this.setState(state => ({
+        counter: state.counter + 1
+      }))
+    }
+  }
+
+  decCounter = () => {
+    if (this.state.counter > -50) {
+      this.setState(state => ({
+        counter: state.counter - 1
+      }))
+    }
+  }
+
+  rndCounter = () => {
+    this.setState({
+      counter: +(Math.random() * (50 - -50) + -50).toFixed(0)
+    })
+  }
+
+  resetCounter = () => {
+    this.setState({
+      counter: this.props.counter
+    })
+  }
+
+  render() {
+    const {counter} = this.state;
+
+    return (
+        <div className="app">
+          <div className="counter">{counter}</div>
+          <div className="controls">
+            <button onClick={this.incCounter}>INC</button>
+            <button onClick={this.decCounter}>DEC</button>
+            <button onClick={this.rndCounter}>RND</button>
+            <button onClick={this.resetCounter}>RESET</button>
+          </div>
+        </div>
+    )
+  }
 }
 
 export default App;
